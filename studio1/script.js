a8f78338b4943de300462b8aa370ae1c2f95f14a
Neha Bagepalli <neha.bagepalli@gmail.com>
2024-10-22 13:26:24 -0700

    js edit studio

--- studio1/script.js
+++ studio1/script.js
@@ -1,12 +1,14 @@
-const button = document.getElementById('bubbleButton');
+(function(){
+    const button = document.getElementById('bubbleButton');
 
-button.addEventListener('click', function() {
-    const bubble = document.createElement('div');
-    const bubble1 = document.createElement('div');
-    bubble.classList.add('bubble');
-    bubble1.classList.add('bubble1');
+    button.addEventListener('click', function() {
+        const bubble = document.createElement('div');
+        const bubble1 = document.createElement('div');
+        bubble.classList.add('bubble');
+        bubble1.classList.add('bubble1');
+        
+        document.body.appendChild(bubble);
+        document.body.appendChild(bubble1);
     
-    document.body.appendChild(bubble);
-    document.body.appendChild(bubble1);
-
-});
+    });
+})();
